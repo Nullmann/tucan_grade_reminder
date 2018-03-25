3da59a6a1c3e61a3244bd6d09e7ee7e1dffe5772
Nullmann <nullmann75@gmx.de>
2018-03-25 19:55:07 +0200

    Sorting on linux works. Included absolute paths for crontab usage without the need to cd

--- main.py
+++ main.py
@@ -38,34 +38,33 @@ browser.open('https://www.tucan.tu-darmstadt.de' + start_page.soup.select('a')[2
 # Navigate to "Module results" page
 browser.follow_link(browser.find_link(url_regex='PRGNAME=COURSERESULTS'))
 
-# Save table with current date and time
-file = open(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')).replace(':', '-') + '.html', 'w')
+# Save table with current date and time as an html-file in the current folder
+path = os.path.dirname(os.path.abspath(__file__)) + '/'
+filename = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')).replace(':', '-') + '.html'
+file = open(path + filename, 'w')
 
-grade_table = str(browser.get_current_page().find_all('table'))
-
-# Strip the table of these weird number which changes with each login
-grade_table = re.sub('N(\d*?)(,|\")', '', grade_table)
+grade_table = browser.get_current_page().find_all('table')
+grade_table = re.sub('N(\d*?)(,|\")', '', str(grade_table)) # Strip the table of these weird number which changes with each login
 file.write(grade_table)
 file.close()
 
 # Get all html-files in the current folder, beginning with the current year
 list_of_files = glob.glob(os.path.dirname(os.path.abspath(__file__)) + '/*' + str(datetime.datetime.now().year) + '*.html')
+list_of_files = sorted(list_of_files,key=os.path.getctime)
 
 # Check if there is more than one item
 if (list_of_files.__len__() == 1):
     print("SKIPPING COMPARISON: There is no file stored to compare to (first time running?)")
 else:
     # Get last grade-table for comparison
-    with open(max(list_of_files[:-1], key=os.path.getctime), 'r') as second_latest_file:
+    with open(list_of_files[-2], 'r') as second_latest_file:
         prev_grade_table = second_latest_file.read()
-
-    with open(max(list_of_files, key=os.path.getctime), 'r') as latest_file:
+    with open(list_of_files[-1], 'r') as latest_file:
         current_grade_table = latest_file.read()
 
     if (prev_grade_table == current_grade_table):
-        print('The two files are the same - No grades have changed and nothing needs to be done anymore')
+        print('The two files are the same - No grades have changed and nothing needs to be done')
     else:
-
         print('Send E-Mail with changed grades')
         msg = MIMEMultipart()
         msg['From'] = CFG.FROM_MAIL
